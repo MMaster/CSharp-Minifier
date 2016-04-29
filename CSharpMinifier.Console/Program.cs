7b63c040b0efbe77a1df1e7c31ef0c1235578f41
MMaster <mmaster@bitbix.com>
2016-04-29 22:46:20 +0200

    Skip argument processing and set best values for SE ingame scripts Introduce way to control the behavior using comments in the code

--- CSharpMinifier.Console/Program.cs
+++ CSharpMinifier.Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace CSharpMinifier.Console
 {
@@ -14,7 +15,7 @@ namespace CSharpMinifier.Console
 				
 			int lineLength = 80;
 
-			if (Array.IndexOf(args, "--line-length") > 0) {
+			if (Array.IndexOf(args, "--line-length") >= 0) {
 				
 				int index = Array.IndexOf(args, "--line-length") + 1;
 				if (index < args.Length) {					
@@ -26,22 +27,38 @@ namespace CSharpMinifier.Console
 			}
 
 			var minifierOptions = new CSharpMinifier.MinifierOptions
-			{
-				LocalVarsCompressing = Array.IndexOf(args, "--locals") > 0,
-				MembersCompressing = Array.IndexOf(args, "--members") > 0,
-				TypesCompressing = Array.IndexOf(args, "--types") > 0,
-				SpacesRemoving = Array.IndexOf(args, "--spaces") > 0,
-				RegionsRemoving = Array.IndexOf(args, "--regions") > 0,
-				CommentsRemoving = Array.IndexOf(args, "--comments") > 0,
-				MiscCompressing = Array.IndexOf(args, "--misc") > 0,
-				ConsoleApp = Array.IndexOf(args, "--console") > 0,
-				NamespacesRemoving = Array.IndexOf(args, "--namespaces") > 0,
+            {
+                LocalVarsCompressing = true,
+                MembersCompressing = true,
+                TypesCompressing = true,
+                SpacesRemoving = true,
+                RegionsRemoving = true,
+                CommentsRemoving = true,
+                MiscCompressing = true,
+                ConsoleApp = true,
+                NamespacesRemoving = false,
+                LineLength = 130,
+                ToStringMethodsRemoving = false,
+                PublicCompressing = true,
+                EnumToIntConversion = false,
+                UselessMembersCompressing = true
+            };
+			/*{
+				LocalVarsCompressing = Array.IndexOf(args, "--locals") >= 0,
+				MembersCompressing = Array.IndexOf(args, "--members") >= 0,
+				TypesCompressing = Array.IndexOf(args, "--types") >= 0,
+				SpacesRemoving = Array.IndexOf(args, "--spaces") >= 0,
+				RegionsRemoving = Array.IndexOf(args, "--regions") >= 0,
+				CommentsRemoving = Array.IndexOf(args, "--comments") >= 0,
+				MiscCompressing = Array.IndexOf(args, "--misc") >= 0,
+				ConsoleApp = Array.IndexOf(args, "--console") >= 0,
+				NamespacesRemoving = Array.IndexOf(args, "--namespaces") >= 0,
 				LineLength = lineLength,
-				ToStringMethodsRemoving = Array.IndexOf(args, "--to-string-methods") > 0,
-				PublicCompressing = Array.IndexOf(args, "--public") > 0,
-				EnumToIntConversion = Array.IndexOf(args, "--enum-to-int") > 0,
-				UselessMembersCompressing = Array.IndexOf(args, "--useless-members") > 0
-			};
+				ToStringMethodsRemoving = Array.IndexOf(args, "--to-string-methods") >= 0,
+				PublicCompressing = Array.IndexOf(args, "--public") >= 0,
+				EnumToIntConversion = Array.IndexOf(args, "--enum-to-int") >= 0,
+				UselessMembersCompressing = Array.IndexOf(args, "--useless-members") >= 0
+			};*/
 
 
 			string filename = args [args.Length - 1];
@@ -51,14 +68,85 @@ namespace CSharpMinifier.Console
 				return;
 			}
 
-			string code = File.ReadAllText (filename);
+			string origcode = File.ReadAllText (filename,System.Text.Encoding.Unicode);
+            string[] lines = origcode.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+            bool skipping = false;
+            List<string> nonMinified = new List<string>();
+            int nonMiniIdx = 0;
+            string code = "";
+            string[] noMiniSymbols = null;
+            string preprocessedCode = "";
 
-			CSharpMinifier.Minifier minifier = new CSharpMinifier.Minifier (minifierOptions);
+            foreach (string line in lines) {
+                string l = line.Trim();
+
+                if (noMiniSymbols == null && l.StartsWith("// NO MINIFY SYMBOLS: ")) {
+                    noMiniSymbols = l.Substring(22).Split(' ');
+                    continue;
+                }
+
+                if (l.StartsWith("// MINIFY INSERT: ")) {
+                    string fname = l.Substring(18);
+                    string insCode = File.ReadAllText(fname, System.Text.Encoding.Unicode);
+                    int afterNsIdx = insCode.IndexOf("{");
+                    if (afterNsIdx < 0) {
+                        System.Console.Error.Write("ERROR: Cannot find { in inserted file: " + fname);
+                        continue;
+                    }
+                    insCode = insCode.Substring(afterNsIdx + 1);
+
+                    int lastBrackIdx = insCode.LastIndexOf("}");
+                    if (lastBrackIdx < 0) {
+                        System.Console.Error.Write("ERROR: Cannot find } in inserted file: " + fname);
+                        continue;
+                    }
+
+                    insCode = insCode.Substring(0, lastBrackIdx);
+                    preprocessedCode += insCode + "\n";
+                    continue;
+                }
+
+                preprocessedCode += l + "\n";
+            }
+
+            lines = preprocessedCode.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+            foreach (string line in lines) {
+                string l = line;
+
+                if (l == "// NO MINIFY }") {
+                    nonMiniIdx++;
+                    skipping = false;
+                    continue;
+                }
+
+                if (skipping) {
+                    nonMinified[nonMiniIdx] += l + "\n";
+                    continue;
+                }
+
+                if (l == "// NO MINIFY {") {
+                    code += "#define MinifierPlaceholder" + nonMiniIdx + "E\n";
+                    nonMinified.Add("");
+                    skipping = true;
+                    continue;
+                }
+                code += l + "\n";
+            }
+
+            CSharpMinifier.Minifier minifier = new CSharpMinifier.Minifier(minifierOptions, noMiniSymbols);
 			string output = minifier.MinifyFromString (code);
 
-			if (Array.IndexOf (args, "--skip-compile") > 0) {
+            output = output.Replace(">==", "> ==");
+
+            nonMiniIdx = 0;
+            foreach (string nonMini in nonMinified) {
+                output = output.Replace("#define MinifierPlaceholder" + nonMiniIdx + "E", "\n" + nonMini);
+                nonMiniIdx++;
+            }
+
+			if (Array.IndexOf (args, "--skip-compile") >= 0) {
 			} else {
-				var compileResult = CSharpMinifier.CompileUtils.Compile (code);
+				var compileResult = CSharpMinifier.CompileUtils.Compile (output);
 
 				if (!compileResult.Errors.HasErrors) {
 					System.Console.Error.WriteLine ("Compiled successfully!");
@@ -72,7 +160,8 @@ namespace CSharpMinifier.Console
 				}
 			}
 
-			System.Console.WriteLine (output);
+            System.IO.File.WriteAllText("script.cs", output, System.Text.Encoding.UTF8);
+			
 		}
 	}
 }
